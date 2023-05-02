aea70ab4766a7627b97411177745c7e5be1e6890
Hyojeong <itshyoj@gmail.com>
2023-05-02 13:47:00 +0900

    feat: get user input (A,B, or C)
    A, B, C or 'end' to exit the game
    Every letter to uppercase

--- monti.py
+++ monti.py
@@ -3,3 +3,7 @@ import random
 def select_door():
     doors = ['A', 'B', 'C']
     return random.choice(doors)
+
+def get_player_choice():
+    return input("Choose a door (A, B, or C), or type 'end' to exit the game: ").upper()
+
